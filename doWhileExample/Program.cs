09f41e12f6c178b48aed832223a7301be2363d25
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-07-18 17:27:46 +0300

    Add project files.

---
+++ doWhileExample/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Bir Sayı Giriniz : ");
+int limit = int.Parse(Console.ReadLine());
+int sayac = 0;
+//While
+while (sayac <= limit - 1)
+{
+
+    Console.WriteLine("Ben Bir Patikalıyım.");
+    sayac++;
+}
+
+//do-while
+do
+{
+
+    sayac++;
+    Console.WriteLine("Ben Bir Patikalıyım");
+
+} while (sayac <= limit-1);
+
+// while döngüsünde koşulu başında kontrol eder programı ona göre çalıştırır . Do-while döngüsünde ise önce program çalışıcaktır, bir kere tur dönecektir. Eğer yanlış ise döngüden çıkar,doğru ise döngüyü devam ettirir.
